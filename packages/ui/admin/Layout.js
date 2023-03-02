5a7b39aac17eff7fae422758f033b6e39ea1e133
Nathan Clevenger <nateclev@gmail.com>
2023-03-02 10:48:42 -0600

    Update Layout.js

--- packages/ui/admin/Layout.js
+++ packages/ui/admin/Layout.js
@@ -16,14 +16,6 @@ import {
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
-const defaultNavigation = [
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
-]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
   { name: 'Settings', href: '#' },
